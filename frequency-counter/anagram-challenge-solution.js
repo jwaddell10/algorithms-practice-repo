ca87d26ad776f959f7fa1bbf14214fb929676911
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-01-15 16:43:21 -0600

    more practice on anagram challenge

--- frequency-counter/anagram-challenge-solution.js
+++ frequency-counter/anagram-challenge-solution.js
@@ -1,28 +1,25 @@
-function validAnagram(string1, string2) {
-	let frequencyCounter1 = {};
-	let frequencyCounter2 = {};
-
-	for (let val of string1) {
-		frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+function validAnagram(first, second) {
+	if (first.length !== second.length) {
+		return false;
 	}
 
-	for (let val of string2) {
-		frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
-	}
-	if (
-		Object.keys(frequencyCounter1).length === 0 &&
-		Object.keys(frequencyCounter2).length === 0
-	) {
-		return true;
+	const lookup = {};
+
+	for (let i = 0; i < first.length; i++) {
+		let letter = first[i];
+		// if letter exists, increment, otherwise set to 1
+		lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
 	}
 
-	for (let key in frequencyCounter1) {
-		if (!(key in frequencyCounter2)) {
+	for (let i = 0; i < second.length; i++) {
+		let letter = second[i];
+		// can't find letter or letter is zero then it's not an anagram
+		if (!lookup[letter]) {
 			return false;
-		}
-		if (frequencyCounter2[key] !== frequencyCounter1[key]) {
-			return false;
+		} else {
+			lookup[letter] -= 1;
 		}
 	}
+
 	return true;
 }
