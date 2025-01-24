efe3727a550e92cad3a2ade278c3f0b1c7d0c036
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-01-24 17:09:50 -0600

    recursion practice

---
+++ recursion/productOfArray.js
@@ -0,0 +1,8 @@
+/*Write a function called productOfArray which takes in an array of
+numbers and returns the product of them all.*/
+
+function productOfArray() {
+}
+
+console.log(productOfArray([1,2,3])) // 6
+console.log(productOfArray([1,2,3,10])) // 60
