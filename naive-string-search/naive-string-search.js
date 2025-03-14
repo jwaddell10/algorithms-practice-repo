45c6835eb566a56279daccb1bc7c7dd30c81aa13
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-03-14 18:39:47 -0500

    bubble sort practice

--- naive-string-search/naive-string-search.js
+++ naive-string-search/naive-string-search.js
@@ -1,7 +1,13 @@
 function naiveSearch(long, short) {
-    
+	let count = 0;
+	for (let i = 0; i < long.length; i++) {
+		for (let j = 0; j < short.length; j++) {
+			if (short[j] !== long[i + j]) break;
+			if (j === short.length - 1) count++;
+		}
+	}
 }
 
 console.log(naiveSearch("lorie loled", "lol"));
-console.log(naiveSearch('ghfsdywwljskadj', 'fdkl'))
-console.log(naiveSearch('jammyammyammy', 'ammy'))
+console.log(naiveSearch("ghfsdywwljskadj", "fdkl"));
+console.log(naiveSearch("jammyammyammy", "ammy"));
