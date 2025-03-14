bb13d6f10d605068bcb66fe94e12b20335ad2d04
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-03-13 20:58:33 -0500

    product of array and reverse str practice

--- recursion/productOfArray.js
+++ recursion/productOfArray.js
@@ -2,7 +2,9 @@
 numbers and returns the product of them all.*/
 
 function productOfArray(array) {
-    
+    if (array.length === 0) return 1;
+
+    return array[0] * productOfArray(array.slice(1))
 }
 
 console.log(productOfArray([1,2,3])) // 6
