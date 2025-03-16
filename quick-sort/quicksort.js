bff3f51988178a7a3cabf5966d8e06c0171c904b
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-03-16 13:38:55 -0500

    quick sort practice

--- quick-sort/quicksort.js
+++ quick-sort/quicksort.js
@@ -1,36 +1,11 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
-	function swap(array, i, j) {
-		let temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
+function pivot() {
 
-	let pivot = arr[start];
-	let swapIndex = start;
-
-	for (let i = start + 1; i < arr.length; i++) {
-		if (pivot > arr[i]) {
-			swapIndex++;
-			swap(arr, swapIndex, i);
-		}
-	}
-	swap(arr, start, swapIndex);
-	return swapIndex;
 }
 
 console.log(pivot([4, 8, 2, 1, 5, 7, 6, 3]));
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
-	if (left < right) {
-		let pivotIndex = pivot(arr, left, right);
-
-		//left
-		quickSort(arr, left, pivotIndex - 1);
-
-		//right
-		quickSort(arr, pivotIndex + 1, right);
-	}
-	return arr;
+function quickSort() {
+	
 }
 
 console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
