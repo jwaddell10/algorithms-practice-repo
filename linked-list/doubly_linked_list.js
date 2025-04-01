83ff60cb1ad6a628e984e718e9f1327eff4facf2
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-04-01 18:05:40 -0500

    doubly linkedlist practice

---
+++ linked-list/doubly_linked_list.js
@@ -0,0 +1,90 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	push(val) {
+		let newNode = new Node(val);
+
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+			this.length++;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+			this.length++;
+		}
+
+		return this;
+	}
+
+	pop() {
+		if (!this.tail) {
+			return undefined;
+		}
+		let poppedNode = this.tail;
+
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.tail = poppedNode.prev;
+			this.tail.next = null;
+			poppedNode.prev = null;
+		}
+		this.length--;
+		return poppedNode;
+	}
+
+	shift() {
+		if (!this.head) return undefined;
+		let oldHead = this.head;
+
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+		} else {
+			this.head = oldHead.next;
+			this.head.prev = null;
+			oldHead.next = null;
+			this.length--;
+			return oldHead;
+		}
+	}
+
+	unshift(val) {
+		let newNode = new Node(val);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.head.prev = newNode;
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+
+		this.length++;
+		return this;
+	}
+}
+
+let list = new DoublyLinkedList();
+
+list.push(1);
+list.push(2);
+list.push(99);
+console.log(list.unshift("unshift"));
+
+console.log(list);
