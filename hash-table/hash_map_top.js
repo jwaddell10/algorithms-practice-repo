8b47a6b74b0595c9e456cfc9bbad2de301766624
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-04-14 16:49:46 -0500

    hashmap practice

--- hash-table/hash_map_top.js
+++ hash-table/hash_map_top.js
@@ -9,15 +9,8 @@ class HashMap {
 		this.size = size;
 	}
 
-	hash(key) {
-		let hashCode = 0;
-
-		const primeNumber = 31;
-		for (let i = 0; i < key.length; i++) {
-			hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.size;
-		}
-
-		return hashCode;
+	hash(name) {
+		return key(i)
 	}
 
 	set(key, value) {
