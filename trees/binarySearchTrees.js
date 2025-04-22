062e26806c2fd451da5e6bfa4aab9b907459d638
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-04-22 16:34:02 -0500

    DFS and BFS for BST

--- trees/binarySearchTrees.js
+++ trees/binarySearchTrees.js
@@ -11,56 +11,76 @@ class BinarySearchTree {
 		this.root = null;
 	}
 
-	insert(val) {
-		let newNode = new Node(val);
+	insert(value) {
+		let newNode = new Node(value);
 
-		if (this.root === null) {
+		if (!this.root) {
 			this.root = newNode;
-		} else {
-			if (val === this.root.value) {
-				return undefined;
-			}
-			let current = this.root;
-			while (true) {
-				if (val < current.value) {
-					if (current.left === null) {
-						current.left = newNode;
-						return this;
-					} else {
-						current = current.left;
-					}
+			return this;
+		}
+		let current = this.root;
+
+		if (value === current.value) return undefined;
+
+		while (true) {
+			if (value > current.value) {
+				if (!current.right) {
+					current.right = newNode;
+					return this;
+				} else {
+					current = current.right;
 				}
-				if (val > current.value) {
-					if (current.right === null) {
-						current.right = newNode;
-						return this;
-					} else {
-						current = current.right;
-					}
+			} else if (value < current.value) {
+				if (!current.left) {
+					current.left = newNode;
+					return this;
+				} else {
+					current = current.left;
 				}
 			}
 		}
 	}
 
-	find(val) {
-		if (this.root === null) return false;
-		
-		let current = this.root;
-		let found = false;
-
-		if (val === this.root.value) return current;
-
-		while(current && !found) {
-			if (val < current.value) {
-				current = current.left;
-			} else if (val > current.value) {
-				current = current.right;
-			} else {
-				found = true;
-			}
+	BFS() {
+		let node = this.root;
+		let data = [];
+		let queue = [];
+		queue.push(this.root);
+		while (queue.length) {
+			node = queue.shift();
+			data.push(node);
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
 		}
-		if (!found) return undefined;
-		return current;
+		return data;
+	}
+
+	DFSPreOrder() {
+		let visitedNodes = [];
+		let current = this.root;
+
+		function traverse(node) {
+			visitedNodes.push(node.value);
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+		}
+
+		traverse(current);
+		return visitedNodes;
+	}
+
+	DFSPostOrder() {
+		let visitedNodes = [];
+		let current = this.root;
+
+		function traverse(node) {
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+			visitedNodes.push(node.value);
+		}
+
+		traverse(current);
+		return visitedNodes;
 	}
 }
 
@@ -76,6 +96,6 @@ tree.insert(13);
 tree.insert(11);
 tree.insert(16);
 tree.insert(10);
-console.log(tree.find(9), 'found')
+console.log(tree.DFS(), "found");
 
-console.log(tree);
+// console.log(tree);
--- trees/binarySearchTrees.solution.js
+++ trees/binarySearchTrees.solution.js
@@ -12,34 +12,66 @@ class BinarySearchTree {
 	}
 
 	insert(val) {
-		const newNode = new Node(val);
+		let newNode = new Node(val);
 
 		if (this.root === null) {
 			this.root = newNode;
 			return this;
-		} else {
-			let current = this.root;
-			if (val === this.root.value) {
-				return undefined;
-			}
-			while (true) {
-				if (val < current.value) {
-					if (current.left === null) {
-						current.left = newNode;
-						return this;
-					} else {
-						current = current.left;
-					}
+		}
+
+		let current = this.root;
+
+		while (true) {
+			if (val === current.value) return undefined;
+
+			if (val < current.value) {
+				if (!current.left) {
+					current.left = newNode;
+					return this;
 				}
-				if (val > current.value) {
-					if (current.right === null) {
-						current.right = newNode;
-						return this;
-					} else {
-						current = current.right;
-					}
+				current = current.left;
+			} else if (val > current.value) {
+				if (!current.right) {
+					current.right = newNode;
+					return this;
 				}
+				current = current.right;
 			}
 		}
+	}
+
+	find(val) {
+		if (this.root === null) return false;
+
+		let current = this.root;
+		let found = false;
+
+		if (val === this.root.value) return current;
+
+		while (current && !found) {
+			if (val < current.value) {
+				current = current.left;
+			} else if (val > current.value) {
+				current = current.right;
+			} else {
+				found = true;
+			}
+		}
+		if (!found) return undefined;
+		return current;
+	}
+
+	BFS() {
+		let node = this.root;
+		data = [];
+		queue = [];
+		queue.push(this.root);
+		while(queue.length) {
+			node = queue.shift();
+			data.push(node);
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
+		}
+		return data;
 	}
 }
