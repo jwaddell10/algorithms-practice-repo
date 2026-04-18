ef3489d4042216cbad930dfcfa3331c415b5846c
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2026-04-18 13:11:28 -0500

    practice ispalindrome

--- two-pointers/isPalindrome.js
+++ two-pointers/isPalindrome.js
@@ -1,24 +1,9 @@
 export function is_palindrome_valid(s) {
-  let left = 0;
-  let right = s.length - 1;
-
-  while (left < right) {
-    while (left < right && !isAlphaNumeric(s[left])) left++;
-    while (left > right && !isAlphaNumeric(s[right])) right--;
-    if (s[left] === s[right]) {
-        left++;
-        right--;
-    }
-
-    if (s[left] !== s[right]) {
-        return false;
-    }
-    left++
-    right--
-  }
-  return true;
 }
 
-const isAlphaNumeric = (c) => /^[a-z0-9]+$/i.test(c);
 
-console.log(is_palindrome_valid("a dog! a panic in a pagoda."))
+console.log(is_palindrome_valid("racecar")) // true
+console.log(is_palindrome_valid("a dog! a panic in a pagoda.")) // true
+console.log(is_palindrome_valid("")) // true
+console.log(is_palindrome_valid("ab")) // false
+console.log(is_palindrome_valid("21.02.2021")) // false
---
+++ two-pointers/isPalindromeSolution.js
@@ -0,0 +1,25 @@
+export function is_palindrome_valid(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (!isAlphaNumeric(s[left])) left++;
+    while (!isAlphaNumeric(s[right])) right--;
+    if (s[left] === s[right]) {
+      left++;
+      right--;
+    } else if (s[left] !== s[right]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const isAlphaNumeric = (c) => /^[a-z0-9]+$/i.test(c);
+
+console.log(is_palindrome_valid("racecar")) // true
+console.log(is_palindrome_valid("a dog! a panic in a pagoda.")) // true
+console.log(is_palindrome_valid("")) // true
+console.log(is_palindrome_valid("ab")) // false
+console.log(is_palindrome_valid("21.02.2021")) // false
