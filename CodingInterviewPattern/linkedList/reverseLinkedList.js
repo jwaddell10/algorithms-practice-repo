5411128e1f338a6da022799ee8215cd865dd1d13
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2026-06-06 17:57:37 -0500

    practice linkedlist

--- CodingInterviewPattern/linkedList/reverseLinkedList.js
+++ CodingInterviewPattern/linkedList/reverseLinkedList.js
@@ -4,21 +4,20 @@ class Node {
 		this.next = next;
 	}
 }
-
 const head = new Node(3, new Node(2, new Node(1)));
 
 function reverseLinkedList(head) {
 	let curr = head;
 	let prev = null;
 
-	while (curr) {
+	while (curr !== null) {
 		const next = curr.next;
 		curr.next = prev;
 		prev = curr;
 		curr = next;
 	}
 
-    return prev;
+	return prev;
 }
 
 console.log(reverseLinkedList(head));
