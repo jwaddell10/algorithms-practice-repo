d0175e5ed4ccc56b0bc1fadd78efee43ac2e0fb5
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2026-06-11 14:54:31 -0500

    RLL practice

--- CodingInterviewPattern/linkedList/reverseLinkedListSolution.js
+++ CodingInterviewPattern/linkedList/reverseLinkedListSolution.js
@@ -1,3 +1,31 @@
+class Node {
+	constructor(val, next = null) {
+		this.val = val;
+		this.next = next;
+	}
+}
 
+const head = new Node(3, new Node(2, new Node(1)))
+
+function reverseLinkedList(head) {
+	let curr = head;
+	const stack = [];
+
+	while (curr) {
+		stack.push(curr)
+		curr = curr.next;
+	}
+
+	let newHead = stack.pop();
+	curr = newHead;
+
+	while (stack.length) {
+		curr.next = stack.pop();
+		curr = curr.next
+	}
+
+	curr.next = null;
+	return newHead;
+}
 
 console.log(reverseLinkedList(head));
