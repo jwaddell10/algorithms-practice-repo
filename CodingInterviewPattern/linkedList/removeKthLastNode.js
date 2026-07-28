360fb39300bb8686fa66d211a33777f9da49aa07
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2026-07-28 18:38:23 -0500

    practice remove kthlastnode

--- CodingInterviewPattern/linkedList/removeKthLastNode.js
+++ CodingInterviewPattern/linkedList/removeKthLastNode.js
@@ -8,23 +8,6 @@ class ListNode {
 const head = new ListNode(1, new ListNode(2, new ListNode(3)));
 
 function removeKthLastNode(head, k) {
-	let dummy = new ListNode(-1);
-	dummy.next = head;
-
-	let leader = dummy;
-	let trailer = dummy;
-
-	for (let i = 0; i < k; i++) {
-		leader = leader.next;
-	}
-	
-	while (leader.next) {
-		leader = leader.next;
-		trailer = trailer.next
-	}
-	
-	trailer.next = trailer.next.next;
-	return dummy.next
 }
 
 console.log(removeKthLastNode(head, 2));
